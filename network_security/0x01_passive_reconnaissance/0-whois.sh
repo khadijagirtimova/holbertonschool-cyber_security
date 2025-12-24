06853079605c1c01cfd5c18861d610a549a83c97
khadijagirtimova <xadica.qirtimova@gmail.com>
2025-12-24 05:44:25 -0500

    task

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' 'BEGIN {OFS=","} /Registrant|Admin|Tech/ {if ($1 ~ /Street/) $2=$2" "; if ($1 ~ /Ext/) $1=$1":"; print $1,$2}' | sed '$d' | awk '{ORS=NR==1?"":RS; print (NR==1?"":prev)} {prev=$0} END {printf "%s", prev}'
+whois Registrant Admin Tech $1 | awk -F': ' 
