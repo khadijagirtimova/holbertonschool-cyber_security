f12eefc64359028f1c8727cce7b2dc562ec31147
khadijagirtimova <xadica.qirtimova@gmail.com>
2026-02-05 09:50:52 -0500

    task

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=~/Desktop/rockyou.txt "$1" && john --show "$1" | cut -d':' -f2 > 4-password.txt
+john --format=raw-md5 --wordlist=~/Desktop/rockyou.txt "$1" && john --format=raw-md5 --show "$1" 2>/dev/null | grep : | cut -d: -f2 > 4-password.txt
