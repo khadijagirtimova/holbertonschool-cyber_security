5eaa3ee5aa3a7f56a1a71f68d96358bb7ee1aa87
khadijagirtimova <xadica.qirtimova@gmail.com>
2026-05-18 13:14:36 -0400

    task

--- scripting_cyber/0x00-ruby_scripting/3-read_file.rb
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -6,5 +6,5 @@ def count_user_ids(path)
   data = JSON.parse(File.read(path))
   counts = data.map { |item| item['userId'] }.tally
   
-  counts.each { |user_id, count| puts "#{user_id}: #{count}" }
+  counts.sort.each { |user_id, count| puts "#{user_id}: #{count}" }
 end
