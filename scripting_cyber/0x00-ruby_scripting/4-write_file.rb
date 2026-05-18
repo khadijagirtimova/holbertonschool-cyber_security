b447f7458efc5616efe611da1d5bd7677696a94b
khadijagirtimova <xadica.qirtimova@gmail.com>
2026-05-18 13:06:33 -0400

    task

---
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+def merge_json_files(file1_path, file2_path)
+  data1 = JSON.parse(File.read(file1_path))
+  data2 = JSON.parse(File.read(file2_path))
+  
+  merged_data = data1 + data2
+  
+  File.write(file2_path, JSON.pretty_generate(merged_data))
+end
